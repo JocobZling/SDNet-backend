d9e2de43f324e6266c0f52e84f65507ad6df1573
zjycxk <91960812+zjycxk@users.noreply.github.com>
2022-03-25 14:38:58 +0800

    Add files via upload

--- src/main/java/cn/sd/entities/vo/UserEmailAndPassword.java
+++ src/main/java/cn/sd/entities/vo/UserEmailAndPassword.java
@@ -10,4 +10,20 @@ import lombok.*;
 public class UserEmailAndPassword {
     String email;
     String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
