3d7e3f539b95da66b2cb40a8f114e292afc38430
Jtata123 <86513833+Jtata123@users.noreply.github.com>
2022-03-25 13:44:06 +0800

    历史记录

---
+++ src/main/java/cn/sd/controllers/HistoryController.java
@@ -0,0 +1,58 @@
+package cn.sd.controllers;
+
+import cn.sd.entities.Detection;
+import cn.sd.entities.User;
+import cn.sd.exceptions.BusinessException;
+
+import cn.sd.services.HistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+import java.util.HashMap;
+//import java.awt.print.Pageable;
+
+@RestController
+@RequestMapping(value = "/face")
+
+public class HistoryController {
+
+
+    private final HistoryService historyService;
+    // 构造方法
+    @Autowired
+    public HistoryController(HistoryService historyService) {
+        this.historyService = historyService;
+    }
+
+
+//    public ResponseEntity getFaceHistoryByUserId(@RequestBody FaceHistory History) {
+//        History hhistory = historyService.findhistory(History);
+//        return new ResponseEntity<>(hhistory, HttpStatus.OK);
+//    }
+
+//    public ResponseEntity<HashMap<String, Object>> getFaceHistoryByUserId(@RequestBody Detection History) throws BusinessException, UnsupportedEncodingException {
+//        return ResponseEntity.ok(HistoryService.findhistory(History.getUserId()));
+
+    @GetMapping(value="/getFaceHistory/pageable")
+    public ResponseEntity getFaceHistoryByUserId(@PageableDefault(sort={"id"},direction= Sort.Direction.DESC)Pageable pageable,
+                                                 @RequestParam(required = false) Long userId
+    )  {
+        Page basicPage=historyService.getObjectivePageable(pageable,userId);
+        return new ResponseEntity<>(basicPage,HttpStatus.OK);
+
+    }
+
+
+
+}
+
+
+
